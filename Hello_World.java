cab39f16fea3363d09939616fec1d1b8f2445313
KY <kylahbrown13@gmail.com>
2024-08-22 11:12:29 -1000

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,22 @@
+/*
+ * first assignment on practicing printing
+ * fcjhedfhf
+ * cr4hifvhct
+ * cfrchfrhcr
+ */
+public class Hello_World {
+
+	public static void main(String[] args) {
+		//prints out "Hello World"
+		System.out.print( "Hello World" );
+		
+		//prints out "Hello World"  again
+		System.out.println("Hello World");
+		
+		//prints out "Hello World" a third time
+		System.out.print("Hello World");
+		
+
+	}
+
+}
