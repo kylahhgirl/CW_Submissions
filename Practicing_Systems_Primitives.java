dc8d08954fdeb9157169ca093cf8a6964a70033c
KY <kylahbrown13@gmail.com>
2024-08-26 09:49:27 -1000

    Update Practicing_Systems_Primitives.java

--- Practicing_Systems_Primitives.java
+++ Practicing_Systems_Primitives.java
@@ -3,3 +3,7 @@
 00         00     0        00000    000000
 0  0       00     0       0     0   0    0
 0    0     00     0000   0       0  0    0
+
+0.53149606299
+
+The circumfrence of the circle is 25.13 and the area of the circle is 50.27.
