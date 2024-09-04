509dd145bfec50d62afb2e9ace8a2a01337f4102
KY <kylahbrown13@gmail.com>
2024-09-04 10:14:12 -1000

    Update Practicing_Systems_Primitives.java

--- Practicing_Systems_Primitives.java
+++ Practicing_Systems_Primitives.java
@@ -1,9 +1,36 @@
-0    0   0    0   0          0      0    0
-0  0      0  0    0         0 0     0    0
-00         00     0        00000    000000
-0  0       00     0       0     0   0    0
-0    0     00     0000   0       0  0    0
+/*
+ * This is the first homework assignment of the school year and I'll be practicing system and primitives.
+ */
+public class Practicing_Systems_Primitives {
 
-0.53149606299
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+ * TASK 1: Print out name using the letter "O" in ASCII art.
+ */
 
-The circumfrence of the circle is 25.13 and the area of the circle is 50.27.
+System.out.println("0   0    0  0   0          00");
+System.out.println("0 0       00    0         0  0");
+System.out.println("00        00    0        000000");
+System.out.println("0 0       00    0       0      0");
+System.out.println("0   0     00    0000   0        0");
+
+/*
+ * TASK 2/3: Write code to compute the math problems.
+ */
+System.out.println((6.0*3.5)-(1.5*5)/(55.6-30.2));
+
+
+final double pi = 3.1419;
+double rad = 4.0;
+double circ = (2.0*pi*rad);
+double area = (pi*(rad*rad));
+
+System.out.println("The circumfrence of the circle is "+ circ +"  and the area is "  +  area + ".");
+
+
+		
+		
+
+	}
+}
