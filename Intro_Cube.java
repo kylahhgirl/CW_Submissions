6cfd788273719c51cc1a27ee9d5c4daa36f53c88
KY <kylahbrown13@gmail.com>
2024-09-20 12:35:06 -1000

    Create Intro_Cube.java

---
+++ Intro_Cube.java
@@ -0,0 +1,23 @@
+
+public class Intro_Cube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x = 4.0;
+		double s = 6*x;
+		double v = x*x*x;
+		
+		System.out.print("side length is ");
+		System.out.println((int)x);
+		
+		System.out.print("side length is ");
+		System.out.println((int)v);
+		
+		System.out.print("the volume of the cube is ");
+		System.out.println((int)s);
+		
+		
+	}
+
+}
