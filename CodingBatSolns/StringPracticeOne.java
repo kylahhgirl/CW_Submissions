a156fc51a9757393fa960f9cd470afdf19ed7db0
KY <kylahbrown13@gmail.com>
2024-11-07 11:10:29 -1000

    Update StringPracticeOne.java

--- CodingBatSolns/StringPracticeOne.java
+++ CodingBatSolns/StringPracticeOne.java
@@ -38,3 +38,17 @@ public String extraEnd(String str) {
   str.substring(str.length()-2) +
   str.substring(str.length()-2);
 }
+
+//firstTwo
+public String firstTwo(String str) {
+  if (str.length() <= 2)
+  {
+    return  str;
+  }
+  return str.substring (0,2);
+}
+
+//withoutEnd
+public String withoutEnd(String str) {
+  return str.substring(1,str.length()-1);
+}
