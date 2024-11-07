ff609e6bf2395d002cfd2e2902ca15a0ca061503
KY <kylahbrown13@gmail.com>
2024-11-07 10:52:54 -1000

    Update LogicPracticeOne.java

--- CodingBatSolns/LogicPracticeOne.java
+++ CodingBatSolns/LogicPracticeOne.java
@@ -33,3 +33,80 @@ else
   return 1;
 }
 }
+
+//SquirrelPlay
+public boolean squirrelPlay(int temp, boolean isSummer) {
+  if (isSummer)
+{
+ if (temp  >= 60 &&temp <= 100)
+  {
+    return true;
+  }
+  
+  }
+  
+  if (!isSummer)
+  {
+    if (temp >=  60 && temp <=  90)
+    {
+        return true;
+    }
+  }
+  return false;
+  }
+
+//caughtSpeeding
+public int caughtSpeeding(int speed, boolean isBirthday) {
+  if (isBirthday)
+{
+ if (speed <= 65)
+ {
+   return 0;
+ }
+
+else if (speed <= 85)
+{
+  return 1;
+}
+
+else 
+{
+  return 2;
+}
+
+}
+
+if (!isBirthday)
+{
+  if (speed <= 60)
+  {
+    return 0;
+  }
+  
+  else if (speed <= 80)
+  {
+    return 1;
+  }
+  
+  else
+  {
+    return 2;
+  }
+}
+return 0;
+}
+
+//sortaSum
+public int sortaSum(int a, int b) {
+  int sum = a+b;
+  
+  if (sum  >= 10 && sum <= 19)
+  {
+    return 20;
+  }
+  
+  else
+  {
+    return sum;
+  }
+}
