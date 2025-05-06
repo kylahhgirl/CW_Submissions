c4fa72caa086993eff2094aad1bf24f6598eef60
KY <kylahbrown13@gmail.com>
2025-05-05 16:22:44 -1000

    Create Player.java

---
+++ InvisibleMaze/Player.java
@@ -0,0 +1,24 @@
+public class Player {
+	
+	private int row;
+	private int column;
+	public Player ()
+	{
+		row = 0;
+		column = 0;
+	}
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	
+}
